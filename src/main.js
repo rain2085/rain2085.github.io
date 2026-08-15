3d9c2370bd4bb6753f830e6b3fa70950d4d7368b
rain2085 <rain_2085@hotmail.com>
2026-08-15 22:40:48 +0800

    重构联系信息显示，更新 HTML 结构和 JavaScript 功能

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-var text;
+var ele;
+const raw = "tempest799165387";
 
-function sclicked(){
-    const h2 = document.querySelector("h2");
-    text = h2.innerText;
-    h2.innerText = "tempest799165387";
+function getContent(date){
+  if(Date.now()>Date.parse(date)){
+    return raw;
+  }
+  else{
+    return raw.replace(/./g, '*');
+  }
 }
 
-function hclicked(){
-    const h2 = document.querySelector("h2");
-    h2.innerText = text;
+const contactInfo = getContent("2026-8-15 22:38:00");
+
+document.addEventListener("DOMContentLoaded",()=>{
+    ele = document.querySelector("#contactInfo");
+    ele.innerText = contactInfo;
+})
+
+function showContent(){
+    ele.innerText = raw;
+}
+
+function hideContent(){
+    ele.innerText = raw.replace(/./g, "*");
 }
